ed124c70715b7d1f9b402712355c28a990ee24d4
teamSuperdiv <team@superdiv.de>
2021-01-30 18:10:04 +0100

    all files related to Kreditcheck400

---
+++ kreditcheck400/assets/js/contact.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 6
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            messages: {
+                name: {
+                    required: "Bitte geben Sie Ihren Vor-und Nachnamen ein",
+                    minlength: "Ihr Name muss aus mindestens 6 Buchstaben bestehen"
+                },
+                subject: {
+                    required: "Bitte geben Sie einen Betreff ein",
+                    minlength: "Der Betreff muss aus mindestens 4 Buchstaben bestehen"
+                },
+                email: {
+                    required: "Bitte geben Sie Ihre E-Mail Adresse ein"
+                },
+                message: {
+                    required: "Bitte geben Sie eine Nachricht ein",
+                    minlength: "Ist das alles?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
---
+++ kreditcheck400/assets/js/mail-script.js
@@ -0,0 +1,31 @@
+    // -------   Mail Send ajax
+
+     $(document).ready(function() {
+        var form = $('#contactForm'); // contact form
+        var submit = $('.button-contactForm'); // submit button
+        var alert = $('.alert-msg'); // alert div for show alert message
+
+        // form submit event
+        form.on('submit', function(e) {
+            e.preventDefault(); // prevent default form submit
+
+            $.ajax({
+                url: 'contact_process.php', // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                data: form.serialize(), // serialize form data
+                beforeSend: function() {
+                    alert.fadeOut();
+                    submit.html('Wird gesendet....'); // change submit button text
+                },
+                success: function(data) {
+                    alert.html(data).fadeIn(); // fade in response data
+                    form.trigger('reset'); // reset form
+                    submit.html('Nachricht wurde gesendet!');; // reset submit button text
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        });
+    });
